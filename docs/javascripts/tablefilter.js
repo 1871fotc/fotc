ac57f45f50d43d7e0d0f0d43e0612cf0d6647768
Stephen-Gates <Stephen.Gates@me.com>
2022-03-27 21:11:23 +1000

    show/hide reset

--- docs/javascripts/tablefilter.js
+++ docs/javascripts/tablefilter.js
@@ -1,34 +1,41 @@
-
-// TODO Not working -  Listen for when Enter is pressed in the Input, and then Search the index //  
-
-/*
-  var listenForEnter = document.getElementById("myInput");
-  listenForEnter.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-       alert('Enter is pressed!');
-//       searchIndex();//
-//     event.preventDefault(); //
-//     document.getElementById("searchButton").click(); //
-    }
-  }); 
-*/  
-
-/* TODO Hack to get working. Note it only works on Desktops */
+// change search button icon and color on Enter //    
+document.addEventListener("keydown", function(event) {
+  if (event.key === 'Enter') {
+// change icon and colour to show search is processing // 
+      document.getElementById("searchButton").style.backgroundColor = "#df41fb"; 
+/*    document.getElementById("searchButton").style.background = "#df41fb url('../assets/hourglass.svg') no-repeat center";
+      document.getElementById("searchButton").style.padding = "10px 10px 10px 10px"; 
+      document.getElementById("searchButton").style.backgroundRepeat = "no-repeat";
+      document.getElementById("searchButton").style.backgroundSize = "24px 24px"; 
+      document.getElementById("searchButton").style.backgroundImage = "url('../assets/hourglass.svg')"; */
+  };
+}),  
+  
+/* TODO Hack to get working - listen to document instead of element. Note it only works on Desktops */
 /* const hark = document.getElementById('myInput'); */
-
 document.addEventListener("keyup", function(event) {
   if (event.key === 'Enter') {
-      document.getElementById("searchButton").style.background = "#df41fb";
       event.preventDefault();
-// change search icon color to hover color //        
       searchIndex();
   };
 // If search input is empty, call clearInput to show the whole table. see https://www.w3schools.com/js/js_validation.asp //
   let x = document.getElementById("myInput").value;
   if (x == "") {
     clearInput();
+    document.getElementById("searchButton").style.backgroundColor = "#7e56c2";
+    document.getElementById("resetButton").style.display = "none"; 
+    // set hover that seems to be lost https://stackoverflow.com/questions/11371550/change-hover-css-properties-with-javascript //
+  }
+  else {
+    document.getElementById("resetButton").style.display = ""; 
   };
 });
+
+// Try change color when search button is pressed - not working //
+function searchProcessing() {
+// change icon and colour to show search is processing //      
+  document.getElementById("searchButton").style.backgroundColor = "#df41fb";
+}
 
 /*
    potential speed increase due to Sarifi poor speed: search only names; 
@@ -41,16 +48,17 @@ document.addEventListener("keyup", function(event) {
 function searchIndex() {
   var input, filter, table, tr, td, i;
   input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
+  filter = input.value.toUpperCase(); 
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
+//  searchProcessing (); //
   for (var i = 1; i < tr.length; i++) {
     var tds = tr[i].getElementsByTagName("td");
     var flag = false;
 // search whole table //    
-    for(var j = 0; j < tds.length; j++){ 
+//    for(var j = 0; j < tds.length; j++){ //
 // only search names //
-//    for(var j = 0; j < 2; j++){ //
+    for(var j = 0; j < 2; j++){ 
       var td = tds[j];
       if (td.innerText.toUpperCase().indexOf(filter) > -1) {
         flag = true;
@@ -64,21 +72,26 @@ function searchIndex() {
     }
   };
 // reset search icon color  //        
-  document.getElementById("searchButton").style.background = "#7e56c2";
+  document.getElementById("searchButton").style.backgroundColor = "#7e56c2"; 
+// document.getElementById("searchButton").style.backgroundImage = "url('../assets/search.svg')"; //
 }
 
-
-
-// When Reset button is pressed, clear the Input, ~~hide the Reset button~~, show all table rows //
+// When Reset button is pressed, clear the Input, Reset search icon color, ~~hide the Reset button~~, show all table rows //
 
   function clearInput() {
     var tr, i;
     document.getElementById("searchButton").style.background = "#7e56c2";
+    document.getElementById("resetButton").style.display = "none"; 
     document.getElementById('myInput').value = '';
-//    document.getElementById('resetButton').style.display = "none"; //
     table = document.getElementById('myTable');
     tr = table.getElementsByTagName("tr");
     for (var i = 1; i < tr.length; i++) {
       tr[i].style.display = "";  
     }
+  }
+
+// hide reset button - call this on load //
+
+  function hideResetButton() {
+    document.getElementById("resetButton").style.display = "none"; 
   }
